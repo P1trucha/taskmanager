14d1bd666d879ffade14fb5aec65e5a12c26699c
P1trucha <piotrmazepka@gmail.com>
2026-03-29 22:19:56 +0200

    fix: Zmiany pod platformę Azure

--- backend/Program.cs
+++ backend/Program.cs
@@ -5,39 +5,46 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Controllers + Swagger
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Database
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+// Dependency Injection
 builder.Services.AddScoped<TaskRepository>();
 builder.Services.AddScoped<TaskService>();
 
-
+// CORS
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
         policy
-            .WithOrigins("http://localhost:8080")
+            .WithOrigins(
+                "http://localhost:8080",
+                "http://localhost:3000",
+                "https://taskmanager-frontend-95269-h0c2g2ezg9gsgsaw.francecentral-01.azurewebsites.net/"
+      
+            )
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
 });
 
-
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// Swagger także na Azure, żeby łatwo testować API
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
+
 app.UseCors("AllowFrontend");
+
 app.MapControllers();
 
 app.Run();
