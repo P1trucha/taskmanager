0a2a3bfc2b83db5ff4160717ec78872012428ce2
P1trucha <piotrmazepka@gmail.com>
2026-04-17 17:20:52 +0200

    fix: Usunięcie hasła z appsettings.json

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,9 +1,28 @@
+using Azure.Identity;
 using backend.Data;
 using backend.Repositories;
 using backend.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Key Vault
+var keyVaultUrl = builder.Configuration["KeyVaultUrl"];
+
+if (!string.IsNullOrWhiteSpace(keyVaultUrl))
+{
+    builder.Configuration.AddAzureKeyVault(
+        new Uri(keyVaultUrl),
+        new DefaultAzureCredential()
+    );
+}
+
+// Connection string:
+// 1. najpierw z Azure Key Vault (sekret: DbConnectionString)
+// 2. fallback do klasycznego ConnectionStrings:DefaultConnection
+var connectionString =
+    builder.Configuration["DbConnectionString"] ??
+    builder.Configuration.GetConnectionString("DefaultConnection");
 
 // Controllers + Swagger
 builder.Services.AddControllers();
@@ -12,7 +31,7 @@ builder.Services.AddSwaggerGen();
 
 // Database
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(connectionString));
 
 // Dependency Injection
 builder.Services.AddScoped<TaskRepository>();
@@ -27,8 +46,7 @@ builder.Services.AddCors(options =>
             .WithOrigins(
                 "http://localhost:8080",
                 "http://localhost:3000",
-                "https://taskmanager-frontend-95269-h0c2g2ezg9gsgsaw.francecentral-01.azurewebsites.net/"
-      
+                "https://taskmanager-frontend-95269-h0c2g2ezg9gsgsaw.francecentral-01.azurewebsites.net"
             )
             .AllowAnyHeader()
             .AllowAnyMethod();
